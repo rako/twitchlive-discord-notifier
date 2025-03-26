83c700408797821287276e1456c439f9585e4baf
rako <69637576+rako@users.noreply.github.com>
2025-03-26 09:25:48 +0900

    2025年03月26日09時25分48秒

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def main():
-    print("Hello World!")
+import requests
+
+discord_token = str(os.environ.get("DISCORD_TOKEN"))
+discord_channel = int(os.environ.get("DISCORD_CHANNEL_ID"))
+
+@functions_framework.http
+def main(request):
+    if request.method == "POST":
+        send(request)
+    
+def send(request):
+    pass
