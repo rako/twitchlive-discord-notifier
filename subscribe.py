9746c872463d94b8ac85489ae9ddfa72793dbc99
rako <69637576+rako@users.noreply.github.com>
2025-03-27 11:42:58 +0900

    サブスクライブをスクリプトにした

---
+++ subscribe.py
@@ -0,0 +1,66 @@
+import functions_framework
+import requests
+import os
+
+@functions_framework.http
+def main(request):
+    if request.method != "POST":
+        return "無効です"
+    
+    subscribe(request)
+
+def subscribe(request):
+    try:
+        json_data = request.get_json()
+
+        if json_data is None:
+            return "JSONデータがありません", 400
+        
+        print(f"受信したJSON: {json_data}") # デバッグ用
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        webhook_url = os.environ.get("WEBHOOK_URL")
+
+        for streamer_info in json_data["data"]:
+            broadcaster_id = streamer_info["broadcaster_id"]
+
+            url = f"https://api.twitch.tv/helix/eventsub/subscriptions"
+            headers = {
+                "Client-ID": os.environ.get("TWITCH_CLIENT_ID"),
+                "Authorization": f"Bearer {os.environ.get('TWITCH_TOKEN')}"
+            }
+
+            # subscribe to online
+            subscribe_json = {
+                "type": "stream.online",
+                "version": "1",
+                "condition": {
+                    "broadcaster_user_id": f"{broadcaster_id}"
+                },
+                "transport": {
+                    "method": "webhook",
+                    "callback": f"{webhook_url}",
+                    "secret": f"{HMAC_SECRET}"
+                }
+            }
+            
+            # subscribe to offline
+            subscribe_json = {
+                "type": "stream.offline",
+                "version": "1",
+                "condition": {
+                    "broadcaster_user_id": f"{broadcaster_id}"
+                },
+                "transport": {
+                    "method": "webhook",
+                    "callback": f"{webhook_url}",
+                    "secret": f"{HMAC_SECRET}"
+                }
+            }
+            
+
+    except Exception as e:
+        print(e)
